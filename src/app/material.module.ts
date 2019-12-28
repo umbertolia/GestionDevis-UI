f02f1d263953a7ceed8a1cab95280b88f4f2d361
umbertolia <bertik@sfr.fr>
2019-12-28 22:15:18 +0100

    ajout module datepicker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,23 @@
 // material.module.ts
 
 import { NgModule } from '@angular/core';
+import { MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
-    
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   exports: [
-    
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ]
 })
 
